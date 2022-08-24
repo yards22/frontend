c06deaf4c33f788486d8de157b55c34ff2f87f58
himanshu-sah <itsmehs07@gmail.com>
2022-08-24 23:10:21 +0530

    deleted unused files

--- Global/Context.ts
+++
@@ -1,8 +0,0 @@
-import { createContext, useContext } from "react";
-import { ISimaraContext } from "./Interface";
-import { DefaultSimaraThemeData } from "./ThemeData";
-
-export const SimaraThemeContext = createContext<ISimaraContext>({
-  themeData: DefaultSimaraThemeData,
-});
-export const useSimara = () => useContext(SimaraThemeContext).themeData;
--- Global/Type.ts
+++
@@ -1,3 +0,0 @@
-type ComponentSize = "small" | "medium" | "large";
-type ComponentIntent = "normal" | "danger" | "success";
-export type { ComponentSize, ComponentIntent };
