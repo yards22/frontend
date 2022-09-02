f251d1ff4d388155e1c546841d9ba4c224de8f6a
himanshu-sah <itsmehs07@gmail.com>
2022-09-03 00:00:42 +0530

    added validation of email in email password login

--- src/Screen/LoginScreen/EmailPasswordLogin.tsx
+++ src/Screen/LoginScreen/EmailPasswordLogin.tsx
@@ -1,17 +1,21 @@
-import { TextInput, Button } from "@mantine/core";
+import { TextInput, Button, Modal } from "@mantine/core";
 import React, { useState } from "react";
 import { IconEye, IconEyeOff } from "../../Atoms/Icons";
 import IconWrapper from "../../Atoms/IconWrapper";
-
+import { ValidateEmail } from "../../Logic/Utils/Validation";
+import ForgotPassword from "./ForgetPassword/Index";
 function EmailPasswordLogin() {
   const [credentials, setCredentials] = useState({ email: "", password: "" });
   const [showPassword, setShowPassword] = useState(false);
+  const [forgotPasswordModal, setForgotPasswordModal] = useState(false);
+
   return (
     <>
       <TextInput
         placeholder="Enter email address of username"
         style={{ width: "100%", marginTop: "20px" }}
         label="Email address or username"
+        type={"email"}
         onChange={(e) => {
           setCredentials((p) => {
             return { ...p, email: e.target.value };
@@ -52,12 +56,32 @@ function EmailPasswordLogin() {
         style={{
           margin: "5px",
         }}
+        onClick={() => {
+          setForgotPasswordModal(true);
+        }}
       >
         Forgot password?
       </Button>
+      <Modal
+        title="Forgot Password"
+        centered
+        transition="fade"
+        transitionDuration={600}
+        transitionTimingFunction="ease"
+        overlayOpacity={0.55}
+        overlayBlur={3}
+        opened={forgotPasswordModal}
+        onClose={() => {
+          setForgotPasswordModal(false);
+        }}
+      >
+        <ForgotPassword />
+      </Modal>
       <Button
         disabled={
-          credentials.email.length === 0 || credentials.password.length === 0
+          credentials.email.length === 0 ||
+          credentials.password.length === 0 ||
+          !ValidateEmail(credentials.email)
         }
         style={{
           marginTop: "20px",
