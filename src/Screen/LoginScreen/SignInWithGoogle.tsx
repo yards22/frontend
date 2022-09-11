fb5f5be73dd0cdef429a3c4f558e8e0ff3f0b54b
himanshu-sah <itsmehs07@gmail.com>
2022-09-11 13:17:33 +0530

    using react oauth lib instead of script import

--- src/Screen/LoginScreen/SignInWithGoogle.tsx
+++ src/Screen/LoginScreen/SignInWithGoogle.tsx
@@ -1,16 +1,10 @@
 import React, { useEffect, useRef, useState } from "react";
 import { useStores } from "../../Logic/Providers/StoresProviders";
 import { Request } from "../../Logic/Utils/Fetch";
-
-const loadScript = (src: any): Promise<void> =>
-  new Promise((resolve, reject) => {
-    if (document.querySelector(`script[src="${src}"]`)) return resolve();
-    const script = document.createElement("script");
-    script.src = src;
-    script.onload = () => resolve();
-    script.onerror = (err) => reject(err);
-    document.body.appendChild(script);
-  });
+import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
+import { Observer } from "mobx-react-lite";
+const CLIENT_ID =
+  "442538559529-bm10qpqtcg3k06rrgnc4i0pqnc3fee4s.apps.googleusercontent.com";
 
 function SignInWithGoogle() {
   const [idToken, setIdToken] = useState<null | string>(null);
@@ -31,38 +25,29 @@ function SignInWithGoogle() {
       });
   }, [idToken]);
 
-  useEffect(() => {
-    const src = "https://accounts.google.com/gsi/client";
-    const id =
-      "442538559529-bm10qpqtcg3k06rrgnc4i0pqnc3fee4s.apps.googleusercontent.com";
-
-    loadScript(src)
-      .then(() => {
-        /*global google*/
-        google.accounts.id.initialize({
-          client_id: id,
-          callback: handleCredentialResponse,
-        });
-        google.accounts.id.renderButton(googleButton.current, {
-          theme: store.appStore.theme == "dark" ? "filled_blue" : "outline",
-          size: "large",
-          width: "300px",
-        });
-      })
-      .catch(console.error);
-
-    return () => {
-      const scriptTag = document.querySelector(`script[src="${src}"]`);
-      if (scriptTag) document.body.removeChild(scriptTag);
-    };
-  }, [store.appStore.theme]);
-
-  function handleCredentialResponse(response: any) {
-    setIdToken(response.credential);
-    console.log("Id Token: " + response.credential);
-  }
-
-  return <div ref={googleButton}></div>;
+  return (
+    <GoogleOAuthProvider clientId={CLIENT_ID}>
+      <Observer>
+        {() => {
+          const { appStore } = store;
+          return (
+            <GoogleLogin
+              size="large"
+              width="300px"
+              theme={appStore.theme == "dark" ? "filled_blue" : "outline"}
+              onSuccess={(credentialResponse) => {
+                console.log(credentialResponse.credential);
+                setIdToken(credentialResponse.credential || null);
+              }}
+              onError={() => {
+                console.log("Login Failed");
+              }}
+            />
+          );
+        }}
+      </Observer>
+    </GoogleOAuthProvider>
+  );
 }
 
 export default SignInWithGoogle;
