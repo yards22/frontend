48f54a44fe1d7ce0e7626697da464d89de603f69
himanshu-sah <itsmehs07@gmail.com>
2022-09-11 19:24:46 +0530

    protected routes inside router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import LoginIndex from "./Screen/LoginScreen/Index";
 import ProvidedApp from "./ProvidedApp";
+import ProtectedRoutes from "./ProtectedRoutes";
+import ProfileIndex from "./Screen/ProfileScreen/Index";
+import LogoutIndex from "./Screen/LogoutScreen/Index";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginIndex from "./Screen/LoginScreen/Index";
+import NetworkIndex from "./Screen/NetworkScreen/Index";
 
 function App() {
   return (
     <ProvidedApp>
-      <LoginIndex />
+      <Router>
+        <Routes>
+          <Route path="/login" element={<LoginIndex />} />
+          <Route element={<ProtectedRoutes />}>
+            <Route path="network" element={<NetworkIndex />} />
+            <Route path="profile" element={<ProfileIndex />} />
+            <Route path="logout" element={<LogoutIndex />} />
+          </Route>
+          <Route>404</Route>
+        </Routes>
+      </Router>
     </ProvidedApp>
   );
 }
