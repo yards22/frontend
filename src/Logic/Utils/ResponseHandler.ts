431ea6f71814cca675b9cb2d639fd2c4e8b08234
himanshu-sah <itsmehs07@gmail.com>
2022-09-11 23:07:44 +0530

    util api call fns init

--- src/Logic/Utils/ResponseHandler.ts
+++ src/Logic/Utils/ResponseHandler.ts
@@ -1,9 +1,9 @@
 class ResponseError extends Error {
-  code: number;
+  status: number;
   bodyMessage: number;
   constructor(code: number, bodyMessage: number) {
     super();
-    this.code = code;
+    this.status = code;
     this.bodyMessage = bodyMessage;
   }
 }
@@ -11,7 +11,7 @@ class ResponseError extends Error {
 export async function CheckResponse(
   res: Response,
   mustCode: number
-): Promise<{ body: any; message: string | null }> {
+): Promise<{ status: number; body: any; message: string | null }> {
   const body = await res.json();
   if (res.status !== mustCode) {
     throw new ResponseError(res.status, body.message || "");
@@ -19,5 +19,13 @@ export async function CheckResponse(
   if (body.is_error) {
     throw new ResponseError(res.status, body.message || "");
   }
-  return { body, message: body.message };
+  return { status: res.status, body, message: body.message };
+}
+
+export function ThrowFor(err: unknown, errFor: { [key: number]: string }) {
+  const error: { status: number; message: string } = {
+    status: (err as any).status,
+    message: (err as any).message || "",
+  };
+  return new Error(errFor[error.status] || "Something went wrong.");
 }
