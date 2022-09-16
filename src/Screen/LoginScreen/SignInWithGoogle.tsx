4320ba30489d3cc5e4282b540a36a2c959bf5a12
sunkoshi <itsmehs07@gmail.com>
2022-09-16 09:41:33 +0530

    handling error case in sign in with google

--- src/Screen/LoginScreen/SignInWithGoogle.tsx
+++ src/Screen/LoginScreen/SignInWithGoogle.tsx
@@ -1,32 +1,17 @@
-import React, { useEffect, useRef, useState } from "react";
 import { useStores } from "../../Logic/Providers/StoresProviders";
-import { Request } from "../../Logic/Utils/Fetch";
 import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
 import { Observer } from "mobx-react-lite";
-import { Outlet } from "react-router-dom";
+import { useState } from "react";
+import { Button } from "@mantine/core";
+import { useNavigate } from "react-router-dom";
 const CLIENT_ID =
   "442538559529-bm10qpqtcg3k06rrgnc4i0pqnc3fee4s.apps.googleusercontent.com";
 
 function SignInWithGoogle() {
-  const [idToken, setIdToken] = useState<null | string>(null);
+  const [didLoginFailed, setDidLoginFailed] = useState(false);
   const store = useStores();
-
-  useEffect(() => {
-    if (idToken == null) return;
-    new Request({})
-      .Post("http://localhost:4000/auth/oauth", {
-        id_token: idToken,
-      })
-      .then(() => {
-        console.log("sent id token");
-        return <Outlet />;
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, [idToken]);
-
-  return (
+  const navigator = useNavigate();
+  return !didLoginFailed ? (
     <GoogleOAuthProvider clientId={CLIENT_ID}>
       <Observer>
         {() => {
@@ -37,18 +22,33 @@ function SignInWithGoogle() {
               width="300px"
               theme={appStore.theme === "dark" ? "filled_blue" : "outline"}
               onSuccess={(credentialResponse) => {
-                setIdToken(credentialResponse.credential || null);
                 if (credentialResponse.credential)
-                  store.authStore.OAuthLoginUser(credentialResponse.credential);
+                  store.authStore
+                    .OAuthLoginUser(credentialResponse.credential)
+                    .then(() => {
+                      navigator("/profile");
+                    })
+                    .catch((err) => setDidLoginFailed(true));
+                else setDidLoginFailed(true);
               }}
               onError={() => {
-                console.log("Login Failed");
+                setDidLoginFailed(true);
               }}
             />
           );
         }}
       </Observer>
     </GoogleOAuthProvider>
+  ) : (
+    <Button
+      color="red"
+      variant="light"
+      onClick={() => {
+        setDidLoginFailed(false);
+      }}
+    >
+      Failed to login with Google. Click to retry.
+    </Button>
   );
 }
 
