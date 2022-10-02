b6149d2ea258b45e9a34e0e8e0f25080aa4c617d
sunkoshi <itsmehs07@gmail.com>
2022-10-02 12:09:15 +0530

    linked user name atom

--- src/Atoms/LinkedUserName.tsx
+++ src/Atoms/LinkedUserName.tsx
@@ -1,8 +1,13 @@
-import { Title, TitleProps, useMantineTheme } from "@mantine/core";
-import React from "react";
+import {
+  Title,
+  TitleProps,
+  Text,
+  useMantineTheme,
+  TextProps,
+} from "@mantine/core";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-const SLinkedUserName = styled(Title)`
+const SLinkedUserNameHard = styled(Title)`
   cursor: pointer;
   color: #282828;
   text-decoration: none;
@@ -10,20 +15,44 @@ const SLinkedUserName = styled(Title)`
     color: ${(p) => p.theme.linkHover};
   }
 `;
-interface LinkedUserNameProps extends TitleProps {
+
+interface LinkedUserNamePropsForHard extends TitleProps {
   username: string;
   user_id?: number;
+  type?: "hard" | "soft";
 }
-function LinkedUserName(props: LinkedUserNameProps) {
+
+interface LinkedUserNamePropsForSoft extends TextProps {
+  username: string;
+  user_id?: number;
+  type?: "hard" | "soft";
+}
+
+function LinkedUserName(
+  props: LinkedUserNamePropsForSoft | LinkedUserNamePropsForHard
+) {
   const mantineTheme = useMantineTheme();
+  if (props.type === "hard")
+    return (
+      <Link to={""} style={{ textDecoration: "none" }}>
+        <SLinkedUserNameHard
+          {...props}
+          theme={{ linkHover: mantineTheme.colors["blue"][7] }}
+        >
+          {props.username}
+        </SLinkedUserNameHard>
+      </Link>
+    );
+
   return (
     <Link to={""} style={{ textDecoration: "none" }}>
-      <SLinkedUserName
-        {...props}
-        theme={{ linkHover: mantineTheme.colors["blue"][7] }}
+      <Text
+        style={{ display: "inline-block", cursor: "pointer" }}
+        {...(props as any)}
+        variant="link"
       >
         {props.username}
-      </SLinkedUserName>
+      </Text>
     </Link>
   );
 }
