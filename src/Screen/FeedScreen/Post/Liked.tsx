668a0bc7e2b3b33eb4b9a251da9ea652cb3de2e5
sunkoshi <itsmehs07@gmail.com>
2022-10-02 12:09:46 +0530

    liked modal

--- src/Screen/FeedScreen/Post/Liked.tsx
+++ src/Screen/FeedScreen/Post/Liked.tsx
@@ -1,9 +1,12 @@
-import React from "react";
-import { Text } from "@mantine/core";
+import React, { useState } from "react";
+import { Modal, Text } from "@mantine/core";
+import LikeList from "./LikeList";
+import LinkedUserName from "../../../Atoms/LinkedUserName";
 interface LikedProps {
   data: string[];
 }
 function Liked(props: LikedProps) {
+  const [showModal, setShowModal] = useState(false);
   if (props.data.length === 0) {
     return (
       <Text color="dimmed" weight={"500"} size="sm">
@@ -14,58 +17,61 @@ function Liked(props: LikedProps) {
   if (props.data.length === 1)
     return (
       <Text color="dimmed" weight={"500"} size="sm">
-        <Text
+        <LinkedUserName
           style={{ display: "inline-block", cursor: "pointer" }}
-          variant="link"
-        >
-          {props.data[0]}
-        </Text>
+          username={props.data[0]}
+        />
         &nbsp; liked it.
       </Text>
     );
   if (props.data.length === 2)
     return (
       <Text color="dimmed" weight={"500"} size="sm">
-        <Text
+        <LinkedUserName
           style={{ display: "inline-block", cursor: "pointer" }}
-          variant="link"
-        >
-          {props.data[0]}
-        </Text>
+          username={props.data[0]}
+        />
         &nbsp; and&nbsp;
-        <Text
+        <LinkedUserName
           style={{ display: "inline-block", cursor: "pointer" }}
-          variant="link"
-        >
-          {props.data[1]}
-        </Text>
+          username={props.data[1]}
+        />
         &nbsp; liked it.
       </Text>
     );
   return (
-    <Text color="dimmed" weight={"500"} size="sm">
-      <Text
-        style={{ display: "inline-block", cursor: "pointer" }}
-        variant="link"
+    <>
+      <Modal
+        opened={showModal}
+        onClose={() => setShowModal(false)}
+        title="Likes"
       >
-        {props.data[0]}
+        <LikeList />
+      </Modal>
+      <Text color="dimmed" weight={"500"} size="sm">
+        <LinkedUserName
+          style={{ display: "inline-block", cursor: "pointer" }}
+          username={props.data[0]}
+        />
+        , &nbsp;
+        <LinkedUserName
+          style={{ display: "inline-block", cursor: "pointer" }}
+          username={props.data[1]}
+        />
+        &nbsp;and &nbsp;
+        <Text
+          style={{ display: "inline-block", cursor: "pointer" }}
+          variant="link"
+          onClick={(e: any) => {
+            e.preventDefault();
+            setShowModal(true);
+          }}
+        >
+          others
+        </Text>
+        &nbsp; liked it.
       </Text>
-      , &nbsp;
-      <Text
-        style={{ display: "inline-block", cursor: "pointer" }}
-        variant="link"
-      >
-        {props.data[1]}
-      </Text>
-      &nbsp;and &nbsp;
-      <Text
-        style={{ display: "inline-block", cursor: "pointer" }}
-        variant="link"
-      >
-        others
-      </Text>
-      &nbsp; liked it.
-    </Text>
+    </>
   );
 }
 
