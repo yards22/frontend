e3c2536355558d2a6e87993bff9daf8fbfa6f920
Saichand819 <saichandgupta819@gmail.com>
2022-12-22 20:10:18 +0530

    Posts section added in profile

--- src/Screen/ProfileScreen/PostsSection/UserFavourites.tsx
+++ src/Screen/ProfileScreen/PostsSection/UserFavourites.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
-import { DummyPosts } from "../../../../Data/Dummies/Post";
-import NormalPost from "../../../FeedScreen/Post/NormalPost";
+import { DummyPosts } from "../../../Data/Dummies/Post";
+import NormalPost from "../../FeedScreen/Post/NormalPost";
 
 const SUserFavourites = styled.div`
   display: flex;
