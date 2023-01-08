6c186a085b5917def33a46b663c8949ad71a2ebc
sunkoshi <itsmehs07@gmail.com>
2023-01-08 23:03:36 +0530

    add repo and store for poll

--- src/Logic/Repository/MiscRepo.ts
+++ src/Logic/Repository/MiscRepo.ts
@@ -1,6 +1,7 @@
 import { AuthHeaders } from "../../Atoms/Util";
+import { MPoll } from "../Model/MPoll";
 import { Request } from "../Utils/Fetch";
-import { CheckResponse } from "../Utils/ResponseHandler";
+import { CheckResponse, ThrowFor } from "../Utils/ResponseHandler";
 export class MiscRepo {
   baseUrl: string;
   rq: Request;
@@ -17,8 +18,6 @@ export class MiscRepo {
 
       // append images to form data
       if (image) {
-        console.log("image is there");
-
         data.append("image", image);
       }
 
@@ -31,6 +30,33 @@ export class MiscRepo {
       await CheckResponse(res, 201);
       return;
     } catch (err) {
+      throw err;
+    }
+  }
+
+  async getPolls(token: string): Promise<MPoll[]> {
+    try {
+      const res = await this.rq.Get(`${this.baseUrl}/poll`, AuthHeaders(token));
+      const { body } = await CheckResponse(res, 200);
+      return body.data;
+    } catch (err: any) {
+      throw err;
+    }
+  }
+
+  async postPollReaction(token: string, poll_id: number, type: number) {
+    try {
+      const res = await this.rq.Post(
+        `${this.baseUrl}/poll`,
+        {
+          poll_id,
+          type,
+        },
+        AuthHeaders(token)
+      );
+      await CheckResponse(res, 201);
+      return;
+    } catch (err: any) {
       throw err;
     }
   }
--- src/Logic/State/MiscStore.ts
+++ src/Logic/State/MiscStore.ts
@@ -1,8 +1,11 @@
 import { action, makeAutoObservable, observable } from "mobx";
+import { MPoll } from "../Model/MPoll";
 import { MiscRepo } from "../Repository/MiscRepo";
 
 export class MiscStore {
   @observable token: string | null = null;
+  @observable polls: MPoll[] | null = null;
+
   miscRepo: MiscRepo;
 
   constructor(miscRepo: MiscRepo) {
@@ -15,6 +18,46 @@ export class MiscStore {
   CreateFeedback = async (content: string, image?: File) => {
     try {
       await this.miscRepo.putFeedback(this.token || "", content, image);
+    } catch (err) {
+      throw err;
+    }
+  };
+
+  @action
+  SetPolls(polls: MPoll[]) {
+    this.polls = polls;
+  }
+
+  @action
+  GetPolls = async () => {
+    try {
+      const polls = await this.miscRepo.getPolls(this.token || "");
+      this.SetPolls(polls);
+    } catch (err) {
+      throw err;
+    }
+  };
+
+  @action
+  PostPollReaction = async (pollId: number, type: number) => {
+    try {
+      await this.miscRepo.postPollReaction(this.token || "", pollId, type);
+      if (this.polls) {
+        const polls = [...this.polls];
+        for (let i = 0; i < polls?.length; i++) {
+          if (polls[i].poll.poll_id === pollId) {
+            polls[i].hasPolled = true;
+            for (let j = 0; j < polls[i].reaction.length; j++) {
+              if (polls[i].reaction[j].type === type) {
+                polls[i].reaction[j].count++;
+                break;
+              }
+            }
+            break;
+          }
+        }
+        this.SetPolls(polls);
+      }
     } catch (err) {
       throw err;
     }
