c93d5d1a2979ed95143dde63e7e22756f902c77b
sunkoshi <itsmehs07@gmail.com>
2023-01-09 10:11:01 +0530

    view all polls page

--- src/Screen/PollsScreen/Index.tsx
+++ src/Screen/PollsScreen/Index.tsx
@@ -1,17 +1,55 @@
-import React, { useEffect } from 'react'
-import { useStores } from '../../Logic/Providers/StoresProviders';
+import { Observer } from "mobx-react-lite";
+import React, { useEffect } from "react";
+import { MPoll } from "../../Logic/Model/MPoll";
+import { useStores } from "../../Logic/Providers/StoresProviders";
+import PollCard from "../../Organs/LeftFooter/Poll/PollCard";
 
-
+function getOptions(poll: MPoll): { title: string; votes: number }[] {
+  const res: { title: string; votes: number }[] = [];
+  poll.poll.options.forEach((option, index) => {
+    let count = 0;
+    poll.reaction.forEach((rx) => {
+      if (rx.type === index) count++;
+    });
+    res.push({ title: option, votes: count });
+  });
+  return res;
+}
 function PollsScreenIndex() {
-    const stores = useStores();
+  const stores = useStores();
 
-    useEffect(()=>{
-        stores.appStore.setNavigationState(6)
-    })
+  useEffect(() => {
+    stores.appStore.setNavigationState(6);
+    if (!stores.miscStore.polls) stores.miscStore.GetPolls();
+  }, []);
 
-    return (
-        <div>PollsScreenIndex</div>
-    )
+  return (
+    <Observer>
+      {() => {
+        const { miscStore } = stores;
+        if (!miscStore.polls) return <p>Loading</p>;
+        return (
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            {miscStore.polls.map((each) => {
+              return (
+                <div
+                  style={{ margin: "10px" }}
+                  key={"poll_page_poll" + each.poll.poll_id}
+                >
+                  <PollCard
+                    hasPolled={each.hasPolled}
+                    pollId={each.poll.poll_id}
+                    question={each.poll.poll_question}
+                    options={getOptions(each)}
+                  />
+                </div>
+              );
+            })}
+          </div>
+        );
+      }}
+    </Observer>
+  );
 }
 
-export default PollsScreenIndex
+export default PollsScreenIndex;
