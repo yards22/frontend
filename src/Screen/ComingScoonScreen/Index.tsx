e3895b248ad312772f73a0f0e1c624fa4f9e2115
sunkoshi <itsmehs07@gmail.com>
2023-01-15 21:07:53 +0530

    add coming soon and feedback in mobile view

--- src/Screen/ComingScoonScreen/Index.tsx
+++ src/Screen/ComingScoonScreen/Index.tsx
@@ -1,16 +1,19 @@
-import React, { useEffect } from 'react'
-import { useStores } from '../../Logic/Providers/StoresProviders'
+import React, { useEffect } from "react";
+import { useStores } from "../../Logic/Providers/StoresProviders";
+import ScoreCarousalIndex from "../../Organs/RightFooter/ScoreCarousal/Index";
 
 function ComingSoonScreenIndex() {
-    const stores = useStores();
+  const stores = useStores();
 
-    useEffect(()=>{
-        stores.appStore.setNavigationState(7)
-    })
+  useEffect(() => {
+    stores.appStore.setNavigationState(7);
+  });
 
-    return (
-        <div>ComingSoonScreenIndex</div>
-    )
+  return (
+    <div style={{ margin: "10px" }}>
+      <ScoreCarousalIndex />
+    </div>
+  );
 }
 
-export default ComingSoonScreenIndex
+export default ComingSoonScreenIndex;
--- src/Screen/FeedbackScreen/Index.tsx
+++ src/Screen/FeedbackScreen/Index.tsx
@@ -1,15 +1,18 @@
-import React, { useEffect } from 'react'
-import { useStores } from '../../Logic/Providers/StoresProviders';
+import React, { useEffect } from "react";
+import { useStores } from "../../Logic/Providers/StoresProviders";
+import FeedbackIndex from "../../Organs/RightFooter/Feedback/Index";
 
 function FeedBackScreenIndex() {
-    const stores = useStores();
+  const stores = useStores();
 
-    useEffect(()=>{
-        stores.appStore.setNavigationState(8)
-    })
-    return (
-        <div>FeedBackScreenIndex</div>
-    )
+  useEffect(() => {
+    stores.appStore.setNavigationState(8);
+  });
+  return (
+    <div style={{ margin: "10px" }}>
+      <FeedbackIndex />
+    </div>
+  );
 }
 
-export default FeedBackScreenIndex
+export default FeedBackScreenIndex;
