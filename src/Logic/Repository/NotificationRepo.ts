5e427291285345ce019da0a6d375a31ad05ea227
sunkoshi <itsmehs07@gmail.com>
2023-01-19 16:07:19 +0530

    update notification store and repo according to new model, add fn to fetch usernames for noti

--- src/Logic/Repository/NotificationRepo.ts
+++ src/Logic/Repository/NotificationRepo.ts
@@ -1,27 +1,52 @@
-import {MNotification} from "../Model/MNotification"
-import { CheckResponse, ThrowFor } from "../Utils/ResponseHandler";
+import {
+  CheckResponse,
+  ThrowFor,
+} from "../Utils/ResponseHandler";
 import { Request } from "../Utils/Fetch";
 import { AuthHeaders } from "../../Atoms/Util";
+import { MNotification } from "../Model/MNotification";
 
-export class NotificationRepo{
-    baseUrl: string;
-    rq: Request;
-    constructor(baseUrl: string, rq: Request) {
-        this.rq = rq;
-        this.baseUrl = baseUrl;
-    }
-    
-    async getNotifications(token:string) {
-       try{
-         const res = await this.rq.Get(this.baseUrl,AuthHeaders(token));
-         const {body} = await CheckResponse(res,200);
-         return body.data as MNotification[];
-       }catch(err:any){
-         ThrowFor(err,{})
-       }
-    }
+export class NotificationRepo {
+  baseUrl: string;
+  rq: Request;
+  constructor(baseUrl: string, rq: Request) {
+    this.rq = rq;
+    this.baseUrl = baseUrl;
+  }
 
-    async updateNotification(){
-        
+  async getNotifications(token: string) {
+    try {
+      const res = await this.rq.Get(
+        this.baseUrl,
+        AuthHeaders(token),
+      );
+      const { body } = await CheckResponse(res, 200);
+      const temp: MNotification[] = (
+        body.data as any[]
+      ).map((v) => {
+        return { ...v, created_at: new Date(v.created_at) };
+      });
+      return temp;
+    } catch (err: any) {
+      ThrowFor(err, {});
     }
+  }
+
+  async getUsernames(token: string, user_ids: number[]) {
+    try {
+      const res = await this.rq.Post(
+        this.baseUrl + "/username",
+        {
+          user_ids,
+        },
+        AuthHeaders(token),
+      );
+      const { body } = await CheckResponse(res, 200);
+      return body.data;
+    } catch (err: any) {
+      ThrowFor(err, {});
+    }
+  }
+
+  async updateNotification() {}
 }
