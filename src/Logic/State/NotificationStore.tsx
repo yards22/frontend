190f2ec40947c9fa4da18ca48a99949b6891f45e
sunkoshi <itsmehs07@gmail.com>
2023-01-19 16:06:08 +0530

    update notification store and repo according to new model, add fn to fetch usernames for noti

--- src/Logic/State/NotificationStore.tsx
+++ src/Logic/State/NotificationStore.tsx
@@ -1,27 +1,29 @@
-import { action, makeAutoObservable, observable } from "mobx";
-import { MNotification, MUINotification } from "../Model/MNotification";
+import {
+  action,
+  makeAutoObservable,
+  observable,
+} from "mobx";
+import {
+  MNotification,
+  MUINotification,
+} from "../Model/MNotification";
 import LinkedUserName from "../../Atoms/LinkedUserName";
 import ago from "s-ago";
-import {
-  dummyNotifications,
-  dummyUserIdUsername,
-} from "../../Data/Dummies/Notification";
+import { dummyUserIdUsername } from "../../Data/Dummies/Notification";
 import { NotificationRepo } from "../Repository/NotificationRepo";
 
 const TOKEN_KEY = "token";
 
 export class NotificationStore {
-  rawNotifications: MNotification[] = [];
   uid_uname: Map<number, string>;
   @observable finalNotifications: MUINotification[] = [];
   @observable isLoading: boolean = false;
-  notificationRepo : NotificationRepo ;
+  notificationRepo: NotificationRepo;
   @observable token: string | null = null;
 
-  constructor(notificationRepo : NotificationRepo ) {
+  constructor(notificationRepo: NotificationRepo) {
     makeAutoObservable(this);
-    this.uid_uname = dummyUserIdUsername;
-    this.rawNotifications = dummyNotifications;
+    this.uid_uname = new Map();
     this.notificationRepo = notificationRepo;
     this.token = window.localStorage.getItem(TOKEN_KEY);
     this.GetNotifications();
@@ -36,23 +38,24 @@ export class NotificationStore {
   GetNotifications = async () => {
     // network call here to get raw notifications
 
-    const rawNotifications = this.notificationRepo.getNotifications(this.token || "");
-    // this.rawNotifications = rawNotifications;
+    const rawNotifications =
+      (await this.notificationRepo.getNotifications(
+        this.token || "",
+      )) || [];
     await this.GetUserNamesFor(
-      this.rawNotifications.map((item) => item.metadata.by)
+      rawNotifications.map(
+        (item) => item.triggered_by_id || 0,
+      ),
     );
 
     const temp: Map<string, MNotification[]> = new Map();
-    this.rawNotifications.forEach((item, index) => {
-      let namespace = `${item.metadata.type}`;
-      if (
-        item.metadata.type === "LIKE" ||
-        item.metadata.type === "COMMENT" ||
-        item.metadata.type === "SHARE"
-      )
-        namespace += `_${item.metadata.post_id}`;
+    rawNotifications.forEach((item, index) => {
+      let namespace = `${item.type}`;
+      if (item.type === "POST")
+        namespace += `_${item.entity_identifier}`;
 
-      if (item.metadata.type === "NEW") namespace += item.id.toString();
+      if (item.type === "NEW")
+        namespace += item.id.toString();
 
       const existing = temp.get(namespace) || [];
       existing.push(item);
@@ -67,72 +70,85 @@ export class NotificationStore {
       let stashes: BigInt[] = [];
       let usernames = (
         <LinkedUserName
-          username={this.uid_uname.get(mapItem[0].metadata.by) || ""}
+          username={
+            this.uid_uname.get(
+              mapItem[0].triggered_by_id || 0,
+            ) || ""
+          }
         />
       );
       if (mapItem.length !== 1) {
         let enough = false;
         usernames = (
           <>
-            {mapItem.map((eachNotification, eachNotificationIndex) => {
-              stashes.push(eachNotification.id);
-              if (eachNotification.status === "Unseen")
-                finalStatus = eachNotification.status;
-              if (eachNotification.created_at > finalDate)
-                finalDate = eachNotification.created_at;
+            {mapItem.map(
+              (eachNotification, eachNotificationIndex) => {
+                stashes.push(eachNotification.id);
+                if (eachNotification.status === "Unseen")
+                  finalStatus = eachNotification.status;
+                if (eachNotification.created_at > finalDate)
+                  finalDate = eachNotification.created_at;
 
-              if (eachNotificationIndex === 3) enough = true;
-              if (enough) return <></>;
-              return (
-                <>
-                  {eachNotificationIndex === mapItem.length - 1
-                    ? " and "
-                    : eachNotificationIndex === 0
-                    ? ""
-                    : ", "}
-                  {eachNotificationIndex === 2 ? (
-                    "others"
-                  ) : (
-                    <LinkedUserName
-                      username={
-                        this.uid_uname.get(eachNotification.metadata.by) || ""
-                      }
-                    />
-                  )}
-                </>
-              );
-            })}
+                if (eachNotificationIndex === 3)
+                  enough = true;
+                if (enough) return <></>;
+                return (
+                  <>
+                    {eachNotificationIndex ===
+                    mapItem.length - 1
+                      ? " and "
+                      : eachNotificationIndex === 0
+                      ? ""
+                      : ", "}
+                    {eachNotificationIndex === 2 ? (
+                      "others"
+                    ) : (
+                      <LinkedUserName
+                        username={
+                          this.uid_uname.get(
+                            eachNotification.triggered_by_id ||
+                              0,
+                          ) || ""
+                        }
+                      />
+                    )}
+                  </>
+                );
+              },
+            )}
           </>
         );
       }
 
       let suffix = "";
-      if (mapItem[0].metadata.type === "LIKE") {
+      if (mapItem[0].type === "LIKE") {
         suffix += " liked your post.";
-      } else if (mapItem[0].metadata.type === "COMMENT") {
+      } else if (mapItem[0].type === "COMMENT") {
         suffix += " commented on your post.";
-      } else if (mapItem[0].metadata.type === "SHARE") {
+      } else if (mapItem[0].type === "SHARE") {
         suffix += " shared your post.";
-      } else if (mapItem[0].metadata.type === "FOLLOW") {
+      } else if (mapItem[0].type === "FOLLOW") {
         suffix += " started to follow you.";
       }
 
       const finalNotification: MUINotification = {
         stashes,
         content:
-          mapItem[0].metadata.type === "NEW" ||
-          mapItem[0].metadata.type === "INFO" ? (
-            mapItem[0].metadata.content
+          mapItem[0].type === "NEW" ||
+          mapItem[0].type === "INFO" ? (
+            mapItem[0].extra.content
           ) : (
             <>
               {usernames} {suffix}
             </>
           ),
         happened: ago(finalDate),
-        type: mapItem[0].metadata.type,
+        type: mapItem[0].type,
         extra: {
-          post_id: mapItem[0].metadata.post_id,
-          redirect_url: mapItem[0].metadata.redirect_url,
+          post_id: mapItem[0].entity_identifier,
+          redirect_url: mapItem[0].extra
+            ? mapItem[0].extra.redirect_url
+            : "",
         },
         status: finalStatus,
       };
@@ -148,14 +164,22 @@ export class NotificationStore {
   GetUserNamesFor = async (ids: number[]) => {
     const fetchUsernameFor: number[] = [];
     ids.forEach((item) => {
-      if (!this.uid_uname.has(item)) fetchUsernameFor.push(item);
+      if (!this.uid_uname.has(item))
+        fetchUsernameFor.push(item);
     });
 
     // network call to get username of fetchUsernameFor
-    const res: string[] = [];
 
-    res.forEach((item, index) => {
-      this.uid_uname.set(fetchUsernameFor[index], item);
+    const res: {
+      user_id: number;
+      username: string;
+    }[] = await this.notificationRepo.getUsernames(
+      this.token || "",
+      ids,
+    );
+
+    res.forEach((v) => {
+      this.uid_uname.set(v.user_id, v.username);
     });
   };
 
