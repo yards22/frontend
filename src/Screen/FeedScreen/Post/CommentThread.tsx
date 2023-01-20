3767d7839cd1e864a562f9e044d160abfc72886f
sunkoshi <itsmehs07@gmail.com>
2023-01-20 16:41:35 +0530

    connect comment ui to store

--- src/Screen/FeedScreen/Post/CommentThread.tsx
+++ src/Screen/FeedScreen/Post/CommentThread.tsx
@@ -1,34 +1,52 @@
-import { DummyComments } from "../../../Data/Dummies/Comment";
-import CommentTile from "../../../Atoms/CommentTile";
-const comments = DummyComments;
-function CommentThread() {
+import { Observer } from "mobx-react-lite";
+import { useEffect } from "react";
+import CommentTile from "./CommentTile";
+import Loading from "../../../Atoms/Loading";
+import { useStores } from "../../../Logic/Providers/StoresProviders";
+interface CommentThreadProps {
+  post_id: bigint;
+}
+function CommentThread(props: CommentThreadProps) {
+  const { commentStore } = useStores();
+  useEffect(() => {
+    commentStore.GetComment(props.post_id);
+  }, []);
   return (
-    <div>
-      {comments.map((item, index) => {
+    <Observer>
+      {() => {
+        const { comments } = commentStore;
+        const show = comments.get(props.post_id);
+        if (!show) return <Loading />;
         return (
-          <div
-            key={`post_comment_inside_${index}}`}
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              marginBottom: "10px",
-            }}
-          >
-            <CommentTile {...item} isChildComment={false} />
-            {item.replies.map((repItem, repIndex) => {
+          <div>
+            {show.map((item, index) => {
               return (
-                <CommentTile
-                  key={`post_comment_${index}_${repIndex}}`}
-                  {...repItem}
-                  isChildComment={true}
-                  style={{ marginLeft: "40px" }}
-                />
+                <div
+                  key={`post_comment_${props.post_id}_${index}`}
+                  className="mb-2 flex flex-col"
+                >
+                  <CommentTile
+                    {...item}
+                    comment_id={item.comment_id}
+                    isReply={false}
+                  />
+                  {item.replies.map((repItem, repIndex) => {
+                    return (
+                      <CommentTile
+                        key={`post_reply_${index}_${props.post_id}_${item.comment_id}_${repIndex}}`}
+                        {...repItem}
+                        comment_id={item.comment_id}
+                        isReply={true}
+                      />
+                    );
+                  })}
+                </div>
               );
             })}
           </div>
         );
-      })}
-    </div>
+      }}
+    </Observer>
   );
 }
 
