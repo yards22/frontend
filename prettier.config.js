490ebfd64f11b3eb4157471679951a8a2700c0fe
sunkoshi <itsmehs07@gmail.com>
2023-01-20 10:21:19 +0530

    change prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -1,8 +1,14 @@
 module.exports = {
-    tabWidth: 2,
-    semi: true,
-    singleQuote: false,
-    printWidth: 60,
-    plugins: [require('prettier-plugin-tailwindcss')],
-    tailwindConfig: './tailwind.config.js',
-  };
+  printWidth: 80,
+  "editor.formatOnSave": true,
+  proseWrap: "always",
+  tabWidth: 2,
+  requireConfig: false,
+  useTabs: false,
+  trailingComma: "none",
+  semi: true,
+  singleQuote: false,
+  printWidth: 60,
+  plugins: [require("prettier-plugin-tailwindcss")],
+  tailwindConfig: "./tailwind.config.js",
+};
