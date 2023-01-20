0723f5d18b0b8b9b47ef244aadab933d30228344
sunkoshi <itsmehs07@gmail.com>
2023-01-20 16:39:52 +0530

    chanage ui of profile photo

--- src/Atoms/ProfilePhoto.tsx
+++ src/Atoms/ProfilePhoto.tsx
@@ -1,26 +1,40 @@
-import { Avatar } from "@mantine/core";
-interface ProfilePhotoProps {
-  userName: string | undefined;
-  profileImageUri: string | null | undefined;
-  style?: React.CSSProperties;
+import { Avatar, MantineNumberSize } from "@mantine/core";
+import { HTMLAttributes } from "react";
+interface ProfilePhotoProps extends HTMLAttributes<HTMLDivElement> {
+  username: string | undefined;
+  profileimageuri: string | null | undefined;
+  size?: MantineNumberSize;
 }
 
 function ProfilePhoto(props: ProfilePhotoProps) {
-  return props.profileImageUri ? (
+  if (props.profileimageuri) {
+    return (
+      <Avatar
+        size={props.size}
+        src={props.profileimageuri}
+        {...props}
+        style={{ borderRadius: "100000px" }}
+      />
+    );
+  }
+  if (props.username)
     <Avatar
-      size={"lg"}
-      src={props.profileImageUri}
-      style={{ ...props.style, borderRadius: "100000px" }}
-    />
-  ) : (
-    <Avatar
-      size={"lg"}
-      src={props.profileImageUri}
+      size={props.size}
+      src={props.profileimageuri}
       color="cyan"
-      style={{ ...props.style, borderRadius: "100000px" }}
+      {...props}
+      style={{ borderRadius: "100000px" }}
     >
-      {props.userName?.substring(0, 2).toUpperCase()}
-    </Avatar>
+      {props.username?.substring(0, 2).toUpperCase()}
+    </Avatar>;
+  return (
+    <Avatar
+      size={props.size}
+      src={props.profileimageuri}
+      color="cyan"
+      {...props}
+      style={{ borderRadius: "100000px" }}
+    />
   );
 }
 
