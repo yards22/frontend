eb5f8dac21dd926fd418e7bcb3e32481df8e2eac
sunkoshi <itsmehs07@gmail.com>
2023-01-20 16:37:17 +0530

    simplified prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -1,14 +1,9 @@
 module.exports = {
-  printWidth: 80,
-  "editor.formatOnSave": true,
-  proseWrap: "always",
   tabWidth: 2,
-  requireConfig: false,
   useTabs: false,
   trailingComma: "none",
   semi: true,
   singleQuote: false,
-  printWidth: 60,
   plugins: [require("prettier-plugin-tailwindcss")],
-  tailwindConfig: "./tailwind.config.js",
+  tailwindConfig: "./tailwind.config.js"
 };
