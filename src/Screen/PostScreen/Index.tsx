b06a1d9978eb4c5f509141c419e50cc721dae963
sunkoshi <itsmehs07@gmail.com>
2023-01-24 17:40:59 +0530

    use hash while sharing post

--- src/Screen/PostScreen/Index.tsx
+++ src/Screen/PostScreen/Index.tsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import Loading from "../../Atoms/Loading";
 import MPost from "../../Logic/Model/MPost";
 import { useStores } from "../../Logic/Providers/StoresProviders";
+import { DeHashWithDate } from "../../Logic/Utils/Common";
 import NormalPost from "../FeedScreen/Post/NormalPost";
 const SPostIndex = styled.section`
   height: 100%;
@@ -15,12 +16,24 @@ function PostIndex() {
   const { postStore } = useStores();
   const search = useLocation().search;
   const queryPostId = new URLSearchParams(search).get("post_id");
+  const queryPostHash = new URLSearchParams(search).get("pr");
   const [loading, setLoading] = useState(true);
   const [post, setPost] = useState<MPost | null>(null);
   useEffect(() => {
-    if (queryPostId && !post) {
+    let findBy = "";
+    if (queryPostId && queryPostId !== "") {
+      // using legacy post_id share
+      findBy = queryPostId;
+    } else if (queryPostHash && queryPostHash !== "") {
+      findBy = DeHashWithDate(queryPostHash);
+    } else {
+      //invalid findBy
+      findBy = "";
+    }
+
+    if (findBy !== "") {
       postStore
-        .GetPostByID(BigInt(queryPostId))
+        .GetPostByID(BigInt(findBy))
         .then((p) => {
           setPost(p);
         })
@@ -28,10 +41,21 @@ function PostIndex() {
         .finally(() => {
           setLoading(false);
         });
+    } else {
+      setLoading(false);
+      setPost(null);
     }
   }, [queryPostId]);
   if (loading) return <Loading />;
-  if (!post) return <p>No post found.</p>;
+  if (!post)
+    return (
+      <p className="mt-10 w-full text-center font-bold">
+        No post found. <br />
+        <span className="font-normal text-gray-700">
+          Either share link has expired or is invalid.
+        </span>
+      </p>
+    );
   return (
     <SPostIndex>
       <NormalPost data={post} />
