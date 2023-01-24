db06ece9e9fe38edb81269a71d268b23d2dcfce2
sunkoshi <itsmehs07@gmail.com>
2023-01-25 01:02:31 +0530

    add color prim to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        prim: {
+          50: "#E7F5FF",
+          100: "#D0EBFF",
+          200: "#A5D8FF",
+          300: "#74C0FC",
+          400: "#4DABF7",
+          500: "#339AF0",
+          600: "#228BE6",
+          700: "#1C7ED6",
+          800: "#1971C2",
+          900: "#1864AB"
+        }
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
