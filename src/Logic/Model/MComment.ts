fdf74dc2cf2e295638166921ddec5149fe6c21df
sunkoshi <itsmehs07@gmail.com>
2023-01-27 15:21:50 +0530

    add type is_own_reply in comment entity

--- src/Logic/Model/MComment.ts
+++ src/Logic/Model/MComment.ts
@@ -5,13 +5,16 @@ export interface MComment {
   user_id: number;
   content: string;
   replies: MCommentReply[];
+  is_own_comment: boolean;
   created_at: Date;
 }
 
 export interface MCommentReply {
+  reply_id: bigint;
   user_id: number;
   username: string;
   profile_image_uri: string | null;
   content: string;
+  is_own_reply: boolean;
   created_at: Date;
 }
