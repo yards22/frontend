ea1bd1b00799d67126d8a4b0f45ffc328eff0646
Saichand819 <saichandgupta819@gmail.com>
2023-03-09 01:18:21 +0530

    model for each ball added

--- src/Logic/Model/MInstantMatch.ts
+++ src/Logic/Model/MInstantMatch.ts
@@ -1,22 +1,3 @@
-export interface MCreatingTheMatchArgs{
-    owner_id:number,
-    visitor_team:string,
-    host_team:string,
-    total_overs:number,
-    venue:string,
-    toss_details:{
-        toss_won_by:string,
-        opted_to:string
-    },
-    players_in_action:{
-       bowler:MBowlerInstantMatch,
-       striker_batsman:MBatsmanInstantMatch,
-       non_striker_batsman:MBatsmanInstantMatch,
-    }
-    current_innings : number,
-    batting_order :string[]
-}
-
 export interface MInstantMatch{
     match_id : number,
     owner_id:number,
@@ -60,3 +41,58 @@ export interface MBowlerInstantMatch{
 }
 
 
+export interface ScoreItem{
+    match_id: string,
+    owner_id:number,
+    innings_details:{
+        innings_id:number,
+        overs:number,
+        balls:number,
+        batting_team:string,
+        bowling_team:string,
+      },
+      players_in_action:{
+        bowler:MBowlerInstantMatch,
+        striker_batsman:MBatsmanInstantMatch,
+        non_striker_batsman:MBatsmanInstantMatch, 
+      },
+      extra_details:{
+        is_extra:boolean,
+        extra_type?:string, 
+      },
+      runs_details:{
+        runs_scored:number,
+        is_boundary?:boolean,
+        boundary_type?:string,
+        scored_by:string,
+        wagon_direction?:string,
+       },
+       wicket_details:{ 
+        is_wicket:boolean,
+        wicket_type?:string,
+        wicket_of?:string,
+        wicket_by?:string,
+        is_fielder?:boolean,
+        fielded_by?:string,
+      },
+      undo:boolean,
+}
+
+export interface MCreatingTheMatchArgs{
+    owner_id:number,
+    visitor_team:string,
+    host_team:string,
+    total_overs:number,
+    venue:string,
+    toss_details:{
+        toss_won_by:string,
+        opted_to:string
+    },
+    players_in_action:{
+       bowler:MBowlerInstantMatch,
+       striker_batsman:MBatsmanInstantMatch,
+       non_striker_batsman:MBatsmanInstantMatch,
+    }
+    current_innings : number,
+    batting_order :string[]
+}
